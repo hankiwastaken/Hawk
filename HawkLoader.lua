b417506735836a37f223167b72621dc8159d21b5
hankiwastaken <212605163+hankiwastaken@users.noreply.github.com>
2026-04-21 17:24:06 +0300

    Update HawkLoader.lua

--- HawkLoader.lua
+++ HawkLoader.lua
@@ -6,7 +6,55 @@
      \___|_  /(____  /\/\_/ |__|_ \  \___|_  /|______/  |______  /
            \/      \/            \/        \/                  \/ 
 
-    Devs: Hanki, Falpearx, Tv1o
+    Devs: Hanki, Falpearx
 ]]
 
-loadstring(game:HttpGet("https://hawkhub.lol", true))("hawkhubneverdies")
+_HawkKey = "pencizurnabayilirim"
+local HawkLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/hankiwastaken/HawkUILib/refs/heads/main/Source/HawkLib.lua"))() 
+local HttpService = game:GetService("HttpService")
+local Notif = HawkLib:AddNotifications()
+
+local success, data = pcall(function()
+	return request({
+		Url = "https://hawkhub.lol",
+		Method = "GET"
+	})
+end)
+
+if success then
+	--Successfull request
+	if data.Body:find("530") then
+		Notif:Notification({
+			Title = "Hawk Server",
+			Description = "The server is down right now. Try again in a bit. Contact Hanki.",
+			Selection = "Error",
+			Animated = true,
+			Cooldown = 5
+		})
+	elseif data.Body:find("502") then
+		Notif:Notification({
+			Title = "Hawk Server",
+			Description = "Server is up but the app isn't responding. Contact Hanki.",
+			Selection = "Error",
+			Animated = true,
+			Cooldown = 5
+		})
+	else
+		local HawkServerStatus = game:HttpGet("https://status.hawkhub.lol")
+		local HawkServerData = HttpService:JSONDecode(HawkServerStatus)
+		
+		if HawkServerData.script_status and HawkServerData.script_status == "online" then
+			loadstring(game:HttpGet("https://hawkhub.lol", true))("hawkhubneverdies")
+		elseif HawkServerData.script_status and HawkServerData.script_status == "offline" then
+			Notif:Notification({
+				Title = "Hawk HUB",
+				Description = "Hawk HUB is down right now. Try again in a bit. Contact Hanki.",
+				Selection = "Error",
+				Animated = true,
+				Cooldown = 5
+			})
+		end	
+	end
+else
+	warn("Failed to fetch site")
+end
